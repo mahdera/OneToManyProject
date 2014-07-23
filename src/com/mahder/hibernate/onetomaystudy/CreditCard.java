594a8851230e61e4a5635d7ac6fc80e33f071136
Mahder Neway <mahder.neway@echoorigin.com>
2014-07-23 10:11:55 -0400

    onetomany
    onetomany

---
+++ src/com/mahder/hibernate/onetomaystudy/CreditCard.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.mahder.hibernate.onetomaystudy;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * @author alemayehu
+ *
+ */
+@Entity
+@Table(name="TBL_CREDIT_CARD")
+public class CreditCard {
+	@Id
+	@Column(name="CREDIT_CARD_ID")
+	@GeneratedValue
+	private int id;
+	
+	@Column(name="CREDIT_CARD_NUMBER")
+	private String creditCardNumber;
+	
+	@Column(name="CREDIT_CARD_NAME")
+	private String creditCardName;
+	
+	@ManyToOne
+	private User user;
+	
+	public CreditCard(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCreditCardNumber() {
+		return creditCardNumber;
+	}
+
+	public void setCreditCardNumber(String creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	public String getCreditCardName() {
+		return creditCardName;
+	}
+
+	public void setCreditCardName(String creditCardName) {
+		this.creditCardName = creditCardName;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+}//end class
